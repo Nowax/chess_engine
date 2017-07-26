c99e8a1e752249c433d9b8107520ad7e968f95b4
Wojciech Nowak <wnowak90@gmail.com>
2017-07-26 10:14:15 +0200

    initial commit

---
+++ src/chess_engine/engine.java
@@ -0,0 +1,13 @@
+package chess_engine;
+
+public class engine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(get());
+	}
+
+	public static String get() {
+		return "asaa";
+	}
+}
---
+++ src/chess_engine/engineTest.java
@@ -0,0 +1,14 @@
+package chess_engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class engineTest {
+
+	@Test
+	public void testGet() {
+		assertEquals(engine.get(), "asaa");
+	}
+
+}
